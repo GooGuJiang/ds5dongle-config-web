12fd0367efbf6f232bf093ab5c5954a97a1e01e2
咕谷酱 <74496778+GooGuJiang@users.noreply.github.com>
2026-05-09 11:47:55 +0800

    update version 0.1.8

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
-const appVersion = "0.1.7";
+const appVersion = "0.1.8";
 
 export default defineConfig({
   plugins: [
